f3c20aa5a208fba6f210c198f2ff152b408e92e5
awais9881 <awais5120270@gmail.com>
2026-03-05 01:02:03 +0500

    Add files via upload

---
+++ LAB 1/LAB TASK/LAB 1 TASK 12.cpp
@@ -0,0 +1,22 @@
+// Name: M.Awais
+// Department: Electrical Engineering (POWER)
+// Registration No: BF25NWELE0732
+// Section: A
+
+
+
+   
+#include <iostream> 
+using namespace std;
+// function main begins program execution
+int main()
+    {
+    float aB = 10.2;//it is use for decimal number
+    
+    cout<< aB;// for display output
+    
+    o
+    
+    return 0;   // indicate that program ended successfully
+    } // end function main
+
---
+++ LAB 1/LAB TASK/LAB 1 TASK 13.cpp
@@ -0,0 +1,18 @@
+// Name: M.Awais
+// Department: Electrical Engineering (POWER)
+// Registration No: BF25NWELE0732
+// Section: A
+
+
+#include <iostream>
+using namespace std;
+// function main begins program execution
+int main()
+    {
+    int a;
+    cout << "a = " << a;// for out put  display  and we enter the number in out put display
+    
+    
+    return 0;   // indicate that program ended successfully
+    } // end function main
+
