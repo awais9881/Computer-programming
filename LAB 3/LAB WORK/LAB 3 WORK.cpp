a73dbc6fcbbfa2df9abc721c103e4c7af23ebd88
awais9881 <awais5120270@gmail.com>
2026-03-05 01:37:37 +0500

    Update and rename LAB 3 PRAC TASK 2.cpp to LAB 3 WORK.cpp

--- LAB 3/LAB WORK/LAB 3 WORK.cpp
+++ LAB 3/LAB WORK/LAB 3 WORK.cpp
@@ -1,8 +1,7 @@
-// Name: Abdul Ahad
-// Department: Electrical Engineering (AI)
-// Registration No: BF25NWELE0692
-// Section: B
-
+// Name: M.Awais
+// Department: Electrical Engineering (POWER)
+// Registration No: BF25NWELE0732
+// Section: A
 
 #include <iostream>
 using namespace std;
@@ -26,3 +25,4 @@ else {
 
 return 0;
 }
+
