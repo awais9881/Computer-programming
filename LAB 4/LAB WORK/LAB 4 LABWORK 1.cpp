9d4f8be8812fbc774f059c87f8ac2686a04bd434
awais9881 <awais5120270@gmail.com>
2026-03-05 02:00:55 +0500

    Update LAB 4 LABWORK 1.cpp

--- LAB 4/LAB WORK/LAB 4 LABWORK 1.cpp
+++ LAB 4/LAB WORK/LAB 4 LABWORK 1.cpp
@@ -4,50 +4,53 @@
 // Section: A
 
 
-#include <iostream>
-using namespace std;
-int main()
+#include <iostream> // Include input-output library
+using namespace std; // Use standard namespace
+
+int main() 
+// Program execution starts here
+	{
+    int day; // Declare variable to store day number
+
+    cout << "Enter day number (1-7): "; // Ask user for input
+    cin >> day; // Take input from user
+
+    switch(day) // Start switch statement
     {
-    	int day=0;
-    	cout<<"enter a number ( 1 - 7)"<<endl;
-    	cin>>day;
-    	
-    	switch(day)
-    {
-			case (1):
-cout<<"monday";
-break;
+        case 1: // If input is 1
+            cout << "Monday"; // Display Monday
+            break; // Exit switch
 
-	
-    	case(2):
-cout<<"tuesday";
-break;
+        case 2:
+            cout << "Tuesday";
+            break;
 
-	case(3):
-cout<<"wednesday";
-break;
+        case 3:
+            cout << "Wednesday";
+            break;
 
-	case(4):
-cout<<"thursday";
-break;
+        case 4:
+            cout << "Thursday";
+            break;
 
-	case(5):
-cout<<"friday";
-break;
+        case 5:
+            cout << "Friday";
+            break;
 
-	case(6):
-cout<<"saturday";
-break;
+        case 6:
+            cout << "Saturday";
+            break;
 
-	case(7):
-cout<<"sunday";
-break;
+        case 7:
+            cout << "Sunday";
+            break;
 
-default:
-	cout<<"enter a valid number";
-	
-}
+        default: // If input is not between 1�7
+            cout << "Invalid day number";
+    }
 
-return 0;
-}
+    return 0; // End program successfully
+	}
+
+
 
