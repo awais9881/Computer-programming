e4de71e61d78e0298e25289f2bc7dacb02d8d7a5
awais9881 <awais5120270@gmail.com>
2026-03-05 01:38:46 +0500

    Update LAB 4 LABWORK 1.cpp

--- LAB 4/LAB WORK/LAB 4 LABWORK 1.cpp
+++ LAB 4/LAB WORK/LAB 4 LABWORK 1.cpp
@@ -1,7 +1,7 @@
-// Name: Abdul Ahad
-// Department: Electrical Engineering (AI)
-// Registration No: BF25NWELE0692
-// Section: B
+// Name: M.Awais
+// Department: Electrical Engineering (POWER)
+// Registration No: BF25NWELE0732
+// Section: A
 
 
 #include <iostream>
@@ -50,3 +50,4 @@ default:
 
 return 0;
 }
+
